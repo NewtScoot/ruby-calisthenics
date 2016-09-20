4ea6ee4c319347309b7a8c9630d717ea40747cc5
Newton McCollum <nmccollu@citadel.edu>
2016-09-20 00:12:38 +0000

    Finished fun_with_string 2/4

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,12 +1,30 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    string = self.downcase.delete("^a-zA-Z")
+    string == string.reverse
   end
+  
   def count_words
-    # your code here
+    wordHash = Hash.new
+    string = self.downcase.gsub(/\s/, " ").delete("^Z-Za-z ")
+    words = string.split(" ")
+    words.each do  |w|
+      if wordHash.key? w
+        wordHash[w] += 1
+      else
+        wordHash[w] = 1
+      end
+    end
+    return wordHash
   end
+  
   def anagram_groups
-    # your code here
+    wordsArr =[]
+    words = self.downcase.split(" ")
+    
+    wordsArr = words.group_by{ |w| w.chars.sort}.values
+    
+    
   end
 end
 
